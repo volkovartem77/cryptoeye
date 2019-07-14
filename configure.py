28c4a9e53e592aed9c5345ed674be705836655b5
Artem O. Volkov <volckov.tyoma@gmail.com>
2019-07-14 11:22:47 +0300

    exchanges added

--- configure.py
+++ configure.py
@@ -5,10 +5,11 @@ PROJECT_PATH = os.path.abspath(os.curdir) + '/'
 PROJECT_FOLDER = PROJECT_PATH.split('/')[-2]
 CONF_PATH = PROJECT_PATH + PROJECT_FOLDER + '.conf'
 
-
 f = open(CONF_PATH, "w")
 text = ''
-programs = ['websockets/wsBibox', 'websockets/wsBinance', 'websockets/wsBitmart', 'monitor']
+programs = ['websockets/wsBibox', 'websockets/wsBinance', 'websockets/wsBitmart',
+            'websockets/wsBittrex', 'websockets/wsHitbtc', 'websockets/wsHuobi',
+            'websockets/wsOkex', 'monitor']
 for program_path in programs:
     block = f'''
 [program:{'e' + program_path.split('/')[-1]}]
@@ -30,10 +31,13 @@ numprocs=1
 f.write(text)
 f.close()
 
-files = ['websockets/wsBibox.py', 'websockets/wsBinance.py', 'websockets/wsBitmart.py']
+files = ['websockets/wsBibox.py', 'websockets/wsBinance.py', 'websockets/wsBitmart.py',
+         'websockets/wsBittrex.py', 'websockets/wsHitbtc.py', 'websockets/wsHuobi.py',
+         'websockets/wsOkex.py', ]
 
 for f in files:
     subprocess.call([f'''sed -i "s%# import sys%import sys%g" "{PROJECT_PATH}{f}"'''], shell=True)
-    subprocess.call([f'''sed -i "s%# sys.path.append(PROJECT_PATH)%sys.path.append('{PROJECT_PATH}')%g" "{f}"'''], shell=True)
+    subprocess.call([f'''sed -i "s%# sys.path.append(PROJECT_PATH)%sys.path.append('{PROJECT_PATH}')%g" "{f}"'''],
+                    shell=True)
 subprocess.call([f'''sed -i "s%/home/kusko/PyProjects/CryptoEye/%{PROJECT_PATH}%g" "config.py"'''], shell=True)
 print('Done')
